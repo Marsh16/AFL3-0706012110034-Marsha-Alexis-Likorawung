159ba9dea3f74c87bbcbf4f4b3626c9bbc9e2aba
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-11 08:47:14 +0700

    Initial Commit

---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/AFL3_0706012110034_Marsha_Alexis_LikorawungApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110034_Marsha_Alexis_LikorawungApp.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110034_Marsha_Alexis_LikorawungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

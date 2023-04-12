9044ae7618c1c7fe59d61c6d51847591f8e30b4a
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-12 22:08:00 +0700

    preview (PreviewProvider)
    masih blm bisa pak/bu, sejak kelas hari selasa 11 april 2023

---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 11/04/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}

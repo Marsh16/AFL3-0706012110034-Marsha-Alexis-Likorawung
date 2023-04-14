9203c5945f635aabf61161eaf681e1c5ef5b5e5f
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-14 08:38:38 +0700

    Chapter 1, Section 4, Step 3

---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 14/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}

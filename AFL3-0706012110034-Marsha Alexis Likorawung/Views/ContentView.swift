2cb7a8997e52c3c34c19cb02cea20d39d97e20aa
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-14 10:01:21 +0700

    Animating Views and Transitions, Section 3, Done

--- AFL3-0706012110034-Marsha Alexis Likorawung/Views/ContentView.swift
+++ AFL3-0706012110034-Marsha Alexis Likorawung/Views/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LandmarkList()
+        //Untuk Melihat Landmark
+//        LandmarkList()
+        
+        //Untuk Melihat Hike
+        VStack {
+            HikeView(hike: ModelData().hikes[0])
+                .padding()
+            Spacer()
+        }
+        
+        //Untuk Melihat Badge
+        //        Badge()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .environmentObject(ModelData())
+        ContentView() .environmentObject(ModelData())
     }
 }

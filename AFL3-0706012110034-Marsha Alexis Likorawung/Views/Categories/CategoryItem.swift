c99a6f2878a67076eefdbd5e41d3acfa6bf01ed5
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-26 14:28:19 +0700

    CHAPTER 3, Section 3

---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/Views/Categories/CategoryItem.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryItem.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 26/04/23.
+//
+
+import SwiftUI
+
+//untuk menampilkan categori item
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+//untuk menampilkan categori item preview
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/Views/Categories/CategoryRow.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryRow.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 26/04/23.
+//
+
+import SwiftUI
+
+//untuk menampilkan categori row
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+//untuk menampilkan categori row preview
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(4))
+        )
+    }
+}

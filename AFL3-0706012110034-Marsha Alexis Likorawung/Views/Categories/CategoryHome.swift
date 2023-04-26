64559c6b02e4ec0813dc9adbf9e0246e51998f28
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-26 14:10:47 +0700

    CHAPTER 3, Section 1

---
+++ AFL3-0706012110034-Marsha Alexis Likorawung/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  AFL3-0706012110034-Marsha Alexis Likorawung
+//
+//  Created by Marsha Likorawung  on 26/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
